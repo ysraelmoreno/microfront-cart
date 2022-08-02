bded88cb2b4f964815cc387e91640683d85588cc
Ysrael Moreno <ysraelmoreno02@gmail.com>
2022-08-02 00:28:45 -0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+const deps = require("./package.json").dependencies;
+module.exports = {
+  output: {
+    publicPath: "https://cart-mf.netlify.app/",
+  },
+
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
+  },
+
+  devServer: {
+    port: 3002,
+    historyApiFallback: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.m?js/,
+        type: "javascript/auto",
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+      {
+        test: /\.(css|s[ac]ss)$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(ts|tsx|js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "cart",
+      filename: "remoteEntry.js",
+      remotes: {
+        home: "home@https://home-mf.netlify.app/remoteEntry.js",
+        pdp: "pdp@https://pdp-mf.netlify.app/remoteEntry.js",
+        cart: "cart@https://cart-mf.netlify.app/remoteEntry.js",
+        addToCart: "addToCart@https://addtocart-mf.netlify.app/remoteEntry.js",
+      },
+      exposes: {
+        "./cart": "./src/cart.js",
+        "./Login": "./src/Login.jsx",
+        "./Minicart": "./src/Minicart.jsx",
+        "./CartContent": "./src/CartContent.jsx",
+      },
+      shared: {
+        ...deps,
+        react: {
+          singleton: true,
+          requiredVersion: deps.react,
+        },
+        "react-dom": {
+          singleton: true,
+          requiredVersion: deps["react-dom"],
+        },
+      },
+    }),
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
